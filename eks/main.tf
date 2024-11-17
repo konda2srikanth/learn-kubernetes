eb349176efd51fb8499deb34d0c4ed6e02be6c0f
Konda Srikanth <konda2srikanth@gmail.com>
2024-11-17 19:13:41 +0530

    Auto Push

--- eks/main.tf
+++ eks/main.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_cluster" "name" {
-  name = "name"
+  name = "mysrikanth"
   role_arn = aws_iam_role.example.arn
 
  vpc_config {
@@ -44,3 +44,48 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceControlle
   role       = aws_iam_role.example.name
 }
 
+
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.example.name
+  node_group_name = "mysrikanth-np-spot"
+  node_role_arn   = aws_iam_role.node-example.arn
+  subnet_ids      = ["subnet-01520a6f2a33cd68d", "subnet-0a29d82b448bca8f1", "subnet-0d5495d057954746b"]
+  instance_types = ["t2.meduim","t2.large"]
+  capacity_type = "SPOT"
+  scaling_config {
+    desired_size = 1
+    max_size     = 3
+    min_size     = 1
+  }
+
+}
+
+resource "aws_iam_role" "node-example" {
+  name = "eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node-example.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node-example.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node-example.name
+}
