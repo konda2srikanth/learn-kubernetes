b337d0f18ae053d48082a46d093ae3da688ca034
Konda Srikanth <konda2srikanth@gmail.com>
2024-11-17 20:21:05 +0530

    Auto Push

--- eks/create.sh
+++ eks/create.sh
@@ -3,3 +3,8 @@ terraform init
 terraform plan 
 sleep 10 
 terraform apply -auto-approve
+
+# Installing Metrics Server 
+aws eks update-kubeconfig --name b58-eks
+kubectl get nodes
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
