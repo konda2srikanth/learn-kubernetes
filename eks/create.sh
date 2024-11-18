88918a6ad4a42131f35e42b7a56ccdb02232ebe4
Konda Srikanth <konda2srikanth@gmail.com>
2024-11-18 21:30:54 +0530

    Auto Push

--- eks/create.sh
+++ eks/create.sh
@@ -8,3 +8,6 @@ terraform apply -auto-approve
 aws eks update-kubeconfig --name mysrikanth
 kubectl get nodes
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+kubectl create ns argocd && true
+kubectl apply -f https://raw.githubusercontent.com/B58-CloudDevOps/learn-kubernetes/refs/heads/main/arogCD/argo.yaml -n argocd 
